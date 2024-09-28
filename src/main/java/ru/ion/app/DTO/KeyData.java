7d3e06760663c6206beca848fc3c2f83c1e98d12
ion <ion2005@mail.ru>
2024-09-28 19:04:02 +0300

    add KeyData

---
+++ src/main/java/ru/ion/app/DTO/KeyData.java
@@ -0,0 +1,12 @@
+package ru.ion.app.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+public class KeyData {
+    String key;
+}
