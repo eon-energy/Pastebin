d0a1f620156ca4be7fba271a14b7a99cb905ad69
ion <ion2005@mail.ru>
2024-10-01 13:02:25 +0300

    update PasteData

--- src/main/java/ru/ion/app/DTO/PasteData.java
+++ src/main/java/ru/ion/app/DTO/PasteData.java
@@ -1,13 +1,19 @@
 package ru.ion.app.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Data
 @ToString
-public class PasteData{
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasteData {
+    @NotBlank(message = "text cannot be empty")
     private String text;
     private LocalDate endDate;
 }
