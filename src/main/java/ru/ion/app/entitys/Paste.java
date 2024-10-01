f3dc51eeaa8cd2a51509c26c7340b1584cb946d6
ion <ion2005@mail.ru>
2024-10-01 13:03:26 +0300

    update Paste

--- src/main/java/ru/ion/app/entitys/Paste.java
+++ src/main/java/ru/ion/app/entitys/Paste.java
@@ -1,15 +1,18 @@
 package ru.ion.app.entitys;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
+
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
 @RequiredArgsConstructor
 
 public class Paste {
@@ -25,4 +28,6 @@ public class Paste {
 
     @Column
     private LocalDate endDate;
+
+
 }
