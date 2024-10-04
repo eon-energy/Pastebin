aaff7a6bb25d44575494b8f7d1125e3973740728
ion <ion2005@mail.ru>
2024-10-04 14:05:09 +0300

    update PasteRepository

--- src/main/java/ru/ion/app/repositories/PasteRepository.java
+++ src/main/java/ru/ion/app/repositories/PasteRepository.java
@@ -3,9 +3,13 @@ package ru.ion.app.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ion.app.entitys.Paste;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface PasteRepository extends JpaRepository<Paste, Long> {
     Optional<Paste> findByKey(String link);
     void deleteByKey(String link);
+    List<Paste> findByEndDateBefore(LocalDate date);
+
 }
