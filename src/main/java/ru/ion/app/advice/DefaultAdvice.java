6c45e6e6a416052f717332ec56b62de68e2afdd2
ion <ion2005@mail.ru>
2024-10-05 19:32:58 +0300

    update DefaultAdvice

--- src/main/java/ru/ion/app/advice/DefaultAdvice.java
+++ src/main/java/ru/ion/app/advice/DefaultAdvice.java
@@ -2,6 +2,8 @@ package ru.ion.app.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,15 +14,11 @@ import ru.ion.app.exception.S3ServiceException;
 
 @ControllerAdvice
 public class DefaultAdvice {
+
     @ExceptionHandler({PasteServiceException.class, S3ServiceException.class})
-    public ResponseEntity<ResponseData> handleException(Exception e) {
-        ResponseData responseData = new ResponseData(e.getMessage());
-        return new ResponseEntity<>(responseData, HttpStatus.BAD_REQUEST);
+    public String handleCustomExceptions(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
+        return "error";
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseData> handleValidationErrors(MethodArgumentNotValidException e) {
-        ResponseData responseData = new ResponseData(e.getMessage());
-        return new ResponseEntity<>(responseData, HttpStatus.BAD_REQUEST);
-    }
 }
